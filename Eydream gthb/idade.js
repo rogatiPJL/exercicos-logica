75e45452e091dfad5e384fcd02c5a0dd705dabce
rogati EYDREAM <eydreamestevam6563@gmail.com>
2025-05-27 17:04:45 -0300

    Verificador de idade

---
+++ Eydream gthb/idade.js
@@ -0,0 +1,7 @@
+let idade = prompt(" Por gentileza, escreva sua idade: ")
+if(idade > 18){
+    alert(" Muito bem, pode continuar com seu afazer")
+
+}else{
+    alert(" Saia daqui criança! Você precisa ser de maior para continuar" )
+}
